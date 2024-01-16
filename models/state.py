e796045c5d7dabc611466a6f29fcdecc53baee6a
husamahmud <husamahmud@gmail.com>
2024-01-17 00:50:46 +0300

    [feat] Update State model

--- models/state.py
+++ models/state.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-import os
 from models.base_model import BaseModel, Base
-from models.city import City
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 import models
+from models.city import City
+import os
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128),
-                  nullable=False)
-    cities = relationship('City',
-                          backref='state')
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state')
 
     if os.getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
