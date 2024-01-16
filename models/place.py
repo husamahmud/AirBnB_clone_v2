1e67312a21ca34adeacaee8d98657a62bb56bdf1
husamahmud <husamahmud@gmail.com>
2024-01-17 00:52:17 +0300

    [feat] Update place model

--- models/place.py
+++ models/place.py
@@ -9,11 +9,18 @@ from models.amenity import Amenity
 import models
 
 place_amenity = Table(
-    'place_amenity', Base.metadata,
-    Column('place_id', String(60), ForeignKey('places.id'),
-           primary_key=True, nullable=False),
-    Column('amenity_id', String(60), ForeignKey('amenities.id'),
-           primary_key=True, nullable=False),
+    'place_amenity',
+    Base.metadata,
+    Column('place_id',
+           String(60),
+           ForeignKey('places.id'),
+           primary_key=True,
+           nullable=False),
+    Column('amenity_id',
+           String(60),
+           ForeignKey('amenities.id'),
+           primary_key=True,
+           nullable=False)
 )
 
 
