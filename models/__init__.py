67e219f2e645258f1c2d4761a39fcf428577e6fb
husamahmud <husamahmud@gmail.com>
2024-01-16 22:21:21 +0300

    [feat] Migrate file storage to use FileStorage || DBStorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from os import getenv
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 
 storage_engine = getenv('HBNB_TYPE_STORAGE')
 
 if storage_engine == 'db':
+    from models.engine.db_storage import DBStorage
+
     storage = DBStorage()
 else:
+    from models.engine.file_storage import FileStorage
+
     storage = FileStorage()
 
 storage.reload()
