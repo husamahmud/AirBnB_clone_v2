fc368d9aff85f9ca33010646f5705e7510079ab3
husamahmud <husamahmud@gmail.com>
2024-02-02 02:26:39 +0300

    Add 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,15 +3,15 @@
 from fabric import task
 from datetime import datetime
 
-now = datetime.now().strftime('%Y%m%d%H%M%S')
-
 
 @task
-def do_pack(c):
+def do_pack():
     """Generates a .tgz archive from the contents of the web_static folder."""
-    dir_name = f'web_static_{now}.tgz'
-    c.local('mkdir -p versions')
-    result = c.local(f'tar -cvzf versions/{dir_name} web_static', warn=True)
-    if result.failed:
+    now = datetime.now().strftime('%Y%m%d%H%M%S')
+    try:
+        dir_name = f'web_static_{now}.tgz'
+        local('mkdir -p versions')
+        local(f'tar -cvzf versions/{dir_name} web_static')
+        return f'versions/{dir_name}'
+    except Exception:
         return None
-    return f'versions/{dir_name}'
