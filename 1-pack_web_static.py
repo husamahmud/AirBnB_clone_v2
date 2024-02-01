d7ed08b5b100be8b71a776362655d78a1fb71e05
husamahmud <husamahmud@gmail.com>
2024-02-02 02:22:52 +0300

    Add 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Compress before sending"""
-from fabric import local, task
+"""Generates a .tgz archive from the contents of the web_static folder."""
+from fabric.api import local, task
 from datetime import datetime
 
 now = datetime.now().strftime('%Y%m%d%H%M%S')
