25f9fef3865d661d6d48eafc9eb1e0ea97b13bf2
husamahmud <husamahmud@gmail.com>
2024-02-02 01:40:46 +0300

    Add 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from fabric import task
+from datetime import date
+from time import strftime
+
+date = date.today()
+
+
+@task()
+def do_pack(c):
+    """Generates a .tgz archive from the contents of the web_static folder."""
+    name = f"{date.year}{date.month}{date.day}{strftime('%I%M%S')}"
+    result = c.local(f"tar cvf web_static_{name}.rgz ./web_static", warn=True)
+    if result.failed:
+        return None
+    return result
