ada117cdc70370069c907f76b1bc8de8f571b928
husamahmud <husamahmud@gmail.com>
2024-02-02 03:20:51 +0300

    Add 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,42 +1,49 @@
 #!/usr/bin/python3
-"""create a .tgz archive from contents of the web_static"""
-from fabric.api import local, task, put, run, env
-from datetime import datetime
-import os
+"""script that generates a .tgz archive from the contents of the web_static"""
 
-env.user = "ubuntu"
-env.hosts = ['54.89.27.151', '18.209.225.187']
+from fabric.api import local, task, env, run, settings, put
+import os
+from datetime import datetime
 
 
 @task
 def do_pack():
-    """pack web_static folder"""
+    """archive web_static"""
     try:
-        cur_date = datetime.now().strftime('%Y%m%d%H%M%S')
-        arch = f'web_static_{cur_date}.tgz'
-        local('mkdir -p versions')
-        local(f'tar -cvzf versions/{arch} web_static')
-        return f'versions/{arch}'
-    except Exception:
+        f_current_time = datetime.now().strftime('%Y%m%d%H%M%S')
+        file_name = f'web_static_{f_current_time}.tgz web_static'
+        local("mkdir -p versions")
+        local(f"tar -cvzf versions/{file_name}")
+        return "versions/"
+    except Exception as e:
         return None
 
 
 @task
 def do_deploy(archive_path):
-    """distributes an archive to your web servers"""
-    file_name = archive_path.split('/')[-1].split('.')[0]
+    """deploy web_static to servers"""
+    env.hosts = ['35.153.79.242', '52.201.164.137']
     if not os.path.exists(archive_path):
         return False
+    try:
+        for host in env.hosts:
+            env.host_string = host
+            filename = archive_path.split('/')[-1]
+            filename = filename.split('.')[0]
+            put(archive_path, '/tmp/')
+            run(f'mkdir -p /data/web_static/releases/{filename}/')
+            run(f'tar -xzf /tmp/{filename}.tgz -C \
+                /data/web_static/releases/{filename}/')
+            run(f'rm /tmp/{filename}.tgz')
+            run(f'mv /data/web_static/releases/{filename}/web_static/* \
+                /data/web_static/releases/{filename}/')
+            run(
+                f'rm -rf /data/web_static/releases/{filename}/web_static')
+            run(f'rm -rf /data/web_static/current')
+            run(f'ln -s /data/web_static/releases/{filename}/ \
+                /data/web_static/current')
+            print('New version deployed!')
 
-    put(archive_path, '/tmp/')
-    run(f'mkdir -p /data/web_static/releases/{file_name}/')
-    run(f'tar -xzf /tmp/{file_name}.tgz -C /data/web_static/releases/{file_name}/')
-    run(f'rm /tmp/{file_name}.tgz')
-    run(f'mv /data/web_static/releases/{file_name}/web_static/* \
-    /data/web_static/releases/{file_name}/')
-    run(f'rm -rf /data/web_static/releases/{file_name}/web_static')
-    run('rm -rf /data/web_static/current')
-    run(f'ln -s /data/web_static/releases/{file_name}/ \
-    /data/web_static/current')
-    print('New version deployed!')
-    return True
+        return True
+    except Exception as e:
+        return False
