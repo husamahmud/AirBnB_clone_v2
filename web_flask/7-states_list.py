926f2d679ef02dc22307582f357cdea3c5d0daa7
Hüsam <husamahmud@gmail.com>
2024-02-18 23:12:50 +0300

    Add 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+
+AirBnB = Flask(__name__)
+
+
+@AirBnB.route('/states_list', strict_slashes=False)
+def states_list():
+    """Displays an HTML page only if n is an integer"""
+    return render_template('7-states_list.html', states=storage.all('State'))
+
+
+@AirBnB.teardown_appcontext
+def teardown_db():
+    """Closes the database again at the end of the request."""
+    storage.close()
+
+
+if __name__ == '__main__':
+    AirBnB.run(host='0.0.0.0', port=5000)
