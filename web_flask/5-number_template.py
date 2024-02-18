b531c8a28a77c6b6a7888560ee919eb2769ef16f
Hüsam <husamahmud@gmail.com>
2024-02-18 20:19:42 +0300

    Add 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,7 +37,7 @@ def number(n):
 
 
 @AirBnB.route('/number_template/<int:n>', strict_slashes=False)
-def number(n):
+def number_template(n):
     """Displays an HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
